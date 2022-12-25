cf6e24a0e5785937cec9de6d047ae1f90c517758
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-12-26 00:17:58 +0300

    add products routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -15,4 +15,31 @@ router.post("/", (req, res, next) => {
   });
 });
 
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  if (id === "special") {
+    res.status(200).json({
+      message: "You discovered the special ID",
+      //returns the id in message
+      id: id,
+    });
+  } else {
+    res.status(200).json({
+      message: "You passed an ID",
+    });
+  }
+});
+
+router.patch("/:productId", (req, res, next) => {
+  res.status(200).json({
+    message: "Updated product!",
+  });
+});
+
+router.delete("/:productId", (req, res, next) => {
+  res.status(200).json({
+    message: "Deleted product!",
+  });
+});
+
 module.exports = router;
