bec9b7b946f9eaa67ca7cb33cf773b873f3a5113
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-12-26 02:40:28 +0300

    add cors headers

--- app.js
+++ app.js
@@ -17,6 +17,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //this will json data easily readable to us
 app.use(bodyParser.json());
 
+//handle cors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 //products middleware
 //here I'm sending all requests to /products to the products route
 //so that I don't have to write /products in the products.js in my routes
