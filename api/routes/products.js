adc23a0b9cd9a6a53fa1750c8c15ac66a08f62dd
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-12-26 09:14:57 +0300

    now I really uploaded a tshirt image

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,7 +2,32 @@ const express = require("express");
 const router = express.Router();
 //multer
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
+//configure how the files are stored
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    //where to store the file
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  },
+});
+// const fileFilter = (req, file, cb) => {
+//   //reject a file
+//   if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+//     cb(null, true);
+//   } else {
+//     cb(null, false);
+//   }
+// };
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  // fileFilter: fileFilter,
+});
 
 const {
   getAllProducts,
