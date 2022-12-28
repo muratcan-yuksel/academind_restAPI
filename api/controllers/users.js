9680b30ad997af446ebdf1abcd02b9121281021a
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-12-28 09:22:32 +0300

    sign in user with jwt token

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,7 +2,9 @@ const User = require("../models/user");
 const asyncWrapper = require("../middleware/async");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
+//create user
 const signup = asyncWrapper(async (req, res) => {
   const existingUser = await User.find({ email: req.body.email });
   if (existingUser.length >= 1) {
@@ -30,7 +32,48 @@ const getUsers = asyncWrapper(async (req, res) => {
   });
 });
 
+//login user
+const login = asyncWrapper(async (req, res) => {
+  const user = await User.find({ email: req.body.email });
+  if (user.length < 1) {
+    return res.status(401).json({
+      message: "Auth failed",
+    });
+  }
+  const result = await bcrypt.compare(req.body.password, user[0].password);
+  if (result) {
+    const token = jwt.sign(
+      {
+        email: user[0].email,
+        userId: user[0]._id,
+      },
+      //in order for this to work, I added the password I've created for my user to the .env file
+      process.env.JWT_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+    return res.status(200).json({
+      message: "Auth successful",
+      token: token,
+    });
+  }
+  res.status(401).json({
+    message: "Auth failed",
+  });
+});
+
+//delete user
+const deleteUser = asyncWrapper(async (req, res) => {
+  await User.remove({ _id: req.params.userId });
+  res.status(200).json({
+    message: "User deleted",
+  });
+});
+
 module.exports = {
   signup,
   getUsers,
+  login,
+  deleteUser,
 };
--- api/models/user.js
+++ api/models/user.js
@@ -7,6 +7,9 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
+    match:
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
   },
   password: { type: String, required: true },
 });
